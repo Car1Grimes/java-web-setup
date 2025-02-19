a2a1093d43f46fd2e3ad5662495603766ba6598a
thaivu <hoangminhthaiv@gmail.com>
2025-02-19 21:46:34 +0700

    MVC model

---
+++ OneDrive/Documents/JavaProjects/todoapp/todoapp/src/main/java/com/todo/dto/TaskDTO.java
@@ -0,0 +1,53 @@
+package com.todo.dto;
+import java.time.LocalDate;
+
+/**
+ * @author hoang
+ *
+ */
+public class TaskDTO {
+	private int id;
+	private String title;
+	private String desc;
+	private LocalDate startDate;
+	private LocalDate dueDate;
+	private String status;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+	public LocalDate getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
---
+++ OneDrive/Documents/JavaProjects/todoapp/todoapp/src/main/java/utils/DBUtils.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class DBUtils {
+    
+
+    public static Connection getConnection(){
+        
+        String url;        
+        if (instance == null || instance.trim().isEmpty()) {
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + ";databaseName=" + dbName;
+        } else{
+            url = "jdbc:sqlserver://" + serverName + ":" + portNumber + "\\" + instance + ";databaseName=" + dbName;
+        }
+        
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Can not load JDBC Driver. Please check your pom file");
+        }
+        
+        try {
+            Connection con = DriverManager.getConnection(url, userID, password);
+            return con;
+        } catch (SQLException ex) {
+            System.out.println("Can not connect SQL Server. Reason: " + ex.getMessage());                        
+        }
+        return null;
+    }
+    
+    
+    private final static String serverName = "localhost";
+    private final static String dbName = "todoapp";
+    private final static String portNumber = "1433";
+    private final static String instance = "";//LEAVE THIS ONE EMPTY IF YOUR SQL IS A SINGLE INSTANCE
+    private final static String userID = "sa";
+    private final static String password = "123456";
+}
